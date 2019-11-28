698fc5c24da65a4f0af23e4c59887769ac1b916a
Kyle Lee <kylelee@kyles-mbp.hal.can.ibm.com>
2019-11-28 11:09:51 -0400

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { HomeComponent } from "./home/home.component";
+import { HeadlinesComponent } from "./headlines/headlines.component";
+import { SourcesComponent } from "./sources/sources.component";
+import { FavoritesComponent } from "./favorites/favorites.component";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeadlinesComponent,
+    SourcesComponent,
+    FavoritesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
